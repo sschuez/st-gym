8abb043be7b752480f241b130fa677eaf14858fe
Stephen Schuez <stephen.schuez@gmail.com>
2022-07-06 16:39:07 -0600

    Create a tooltip controller form scratch

---
+++ app/javascript/controllers/tooltip_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="tooltip"
+export default class extends Controller {
+  // static targets = [ 'element', 'tooltip' ]
+  // static values = {
+  //   placement: { type: String, default: "top" },
+  //   offset: { type: Array, default: [0, 8] },
+  // }
+
+  connect() {
+    this.element.addEventListener("mouseenter", show)
+    this.element.addEventListener("mouseleave", hide)
+    
+    function show(event) {
+      var tooltip = document.createElement('div')
+      tooltip.id = "tooltip"
+      tooltip.role = "tooltip"
+      tooltip.innerHTML = event.target.dataset.value
+      
+      var tooltipArrow = document.createElement('div')
+      tooltipArrow.id = "arrow"
+      tooltip.appendChild(tooltipArrow)
+      
+      var parentNode = event.target.parentNode
+      event.target.classList.add("tooltip-parent")
+      parentNode.insertBefore(tooltip, event.target)
+      
+      tooltip.setAttribute("data-show", "")
+  
+      console.log(event.target)
+    }
+
+    function hide(event) {
+      var tooltip = document.getElementById('tooltip')
+      tooltip.remove()
+      event.target.classList.remove("tooltip-parent")
+  
+      console.log(event.target)
+    }
+
+  }
+
+}
