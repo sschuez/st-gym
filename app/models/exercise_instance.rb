978875e11315bd72497e9d75dafa610c5611be67
Stephen Schuez <stephen.schuez@gmail.com>
2022-08-03 17:02:01 -0600

    Broadcast exercise instances to block

--- app/models/exercise_instance.rb
+++ app/models/exercise_instance.rb
@@ -5,4 +5,13 @@ class ExerciseInstance < ApplicationRecord
   delegate :session, to: :block
 
   enum quantity: { more: 1, less: -1, cancel: 0 }
+
+  # broadcasts_to :exercise_instances
+  after_create_commit do
+    broadcast_append_to block, :exercise_instances, target: "block_#{block.id}_exercise_instances", partial: "exercise_instances/exercise_instance"
+  end
+
+  # after_update_commit do
+  #   broadcast_replace_to block, :exercise_instances, target: "block_#{block.id}_exercise_instances", partial: "exercise_instances/exercise_instance"
+  # end
 end
