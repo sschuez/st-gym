55a4370e7bcb9a36f503116f465cee402699b3fb
Stephen Schuez <stephen.schuez@gmail.com>
2022-08-14 11:32:57 -0600

    Add delete button to exercise instance

--- app/controllers/exercise_instances_controller.rb
+++ app/controllers/exercise_instances_controller.rb
@@ -10,33 +10,32 @@ class ExerciseInstancesController < ApplicationController
   
   def create
     exercises = params[:exercise_instance][:exercise_id]
-    
-    exercises.each do |exercise|
-      unless exercise == ""  
-        
-        @exercise_instance = @block.exercise_instances.new(
-          exercise: Exercise.find(exercise.to_i)
-        )
-        respond_to do |format|
-          if @exercise_instance.save
-            format.turbo_stream { flash.now[:notice] = "Exercise instance was successfully created." }
-            # format.html { redirect_to @workout, notice: "Exercise instance was successfully created." }
-            # format.json { render :show, status: :created, location: @exercise_instance }
-          else
-            format.turbo_stream do
-              render turbo_stream: turbo_stream.update(
-                'new_exercise_instance',
-                partial: "exercise_instances/form_modal",
-                locals: {exercise_instance: @exercise_instance})
-            end
-            # format.html { render :new, status: :unprocessable_entity }
-            # format.json { render json: @exercise_instance.errors, status: :unprocessable_entity }
+    exercises.reject { |e| e.empty? }.each do |exercise|
+      @exercise_instance = @block.exercise_instances.new(
+        exercise: Exercise.find(exercise.to_i)
+      )
+      respond_to do |format|
+        if @exercise_instance.save
+          format.turbo_stream { flash.now[:notice] = "Exercise instance was successfully created." }
+          # format.html { redirect_to @workout, notice: "Exercise instance was successfully created." }
+          # format.json { render :show, status: :created, location: @exercise_instance }
+        else
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.update(
+              'new_exercise_instance',
+              partial: "exercise_instances/form_modal",
+              locals: {exercise_instance: @exercise_instance})
           end
+          # format.html { render :new, status: :unprocessable_entity }
+          # format.json { render json: @exercise_instance.errors, status: :unprocessable_entity }
         end
-
       end
     end
+  end
 
+  def destroy
+    @exercise_instance = ExerciseInstance.find(params[:id])
+    @exercise_instance.destroy
   end
 
   private
--- app/models/exercise_instance.rb
+++ app/models/exercise_instance.rb
@@ -9,17 +9,22 @@ class ExerciseInstance < ApplicationRecord
   # validates :repetitions, :time, numericality: { bigger_or_equal_to: 0 }
 
   after_create_commit do
-    broadcast_append_to block, :exercise_instances, 
+    broadcast_append_later_to block, :exercise_instances, 
       target: "block_#{block.id}_exercise_instances", 
       partial: "exercise_instances/exercise_instance", 
       locals: { exercise_instance: self }
   end
 
   after_update_commit do
-    broadcast_replace_to block, :exercise_instances, 
+    broadcast_replace_later_to block, :exercise_instances, 
       target: "exercise_instance_#{self.id}", 
       partial: "exercise_instances/exercise_instance",
       locals: { exercise_instance: self }
+  end
+
+  after_destroy_commit do
+    broadcast_remove_to block, :exercise_instances,
+      target: "exercise_instance_#{self.id}"
   end
   
 end
