6719ad2eff3dafb695dc0b16996b016f92b1f1ad
Stephen Schuez <stephen.schuez@gmail.com>
2022-08-23 18:23:35 -0500

    Remove default domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   # before_action :set_current_user, if: :user_signed_in?
 
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
-  
+  # def default_url_options
+  #   { host: ENV["DOMAIN"] || "localhost:3000" }
+  # end
+
   private
 
   # def set_current_user
