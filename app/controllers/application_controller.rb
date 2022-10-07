f64386df2c6b84ad51e0d4ce737246cd8da3d3a7
Stephen Schuez <stephen.schuez@gmail.com>
2022-10-07 11:13:16 +0200

    Keep previous path after user signs in or out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
   include Pundit::Authorization
   # before_action :set_current_user, if: :user_signed_in?
@@ -15,6 +16,23 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
