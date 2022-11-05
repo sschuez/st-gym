edcd9362283d31017a151699804e84b65cc796f3
Stephen Schuez <stephen.schuez@gmail.com>
2022-11-05 14:21:06 +0100

    Create table for workouts pdf

--- app/models/workout_pdf.rb
+++ app/models/workout_pdf.rb
@@ -11,41 +11,62 @@ class WorkoutPdf
     display_header
     move_down 25
     display_blocks
-    # display_order_created_at
-    
-    
-    # display_items
   end
   
   private
   
   def display_header
-    text @workout.name, size: 40
+    text @workout.name, size: 40, color: '7E73FF'
     text @workout.description, size: 20
+    move_down 20
   end
   
   def display_blocks
     # draw_text "Order #: #{@order.order_number}", at: [400, 712]
     blocks = @workout.blocks.order(position: :asc)
     blocks.each do |block| 
-      text "#{block.title} #{instance_repetitions(block)}", size: 30
+      # text "#{block.title} #{instance_repetitions(block)}", size: 30, color: 'f2c94c'
+      text(
+        "<color rgb='f2c94c'>#{block.title}</color>#{instance_repetitions(block)}",
+        size: 30,
+        # color: '0000FF',
+        inline_format: true
+      )
+      
+      # table([ [block.title, instance_repetitions(block)] ])
+      
+      move_down 10
       block_exercises(block)
-      move_down 15
+      move_down 20
+      
     end
   end
-  
+
   def block_exercises(block)
+    array = []
+    array << ["Exercise", { content: "Repetitions", align: :right }]
+    exercise_instances(array, block)
+    # array << total_price
+    table(array, column_widths: [400, 125]) do
+      cells.borders = []
+      row(0).borders = [:bottom]
+    end
+  end
+
+  def exercise_instances(array, block)
     block.exercise_instances.includes(:exercise).each do |exercise_instance|
-      text "#{exercise_instance.exercise.name} #{instance_repetitions(exercise_instance)}", size: 20
-      move_down 5
+      array << [
+        exercise_instance.exercise.name,
+        { content: instance_repetitions(exercise_instance), align: :right }
+      ]
     end
   end
 
   def instance_repetitions(instance)
     if instance.tabata
-      "  -  #{formatted_duration(instance.time)}"
+      formatted_duration(instance.time)
     else
-      "  -  #{instance.repetitions}x"
+      "#{instance.repetitions}x"
     end
   end
 
@@ -63,26 +84,6 @@ class WorkoutPdf
   #   draw_text @order.created_at, at: [400, 698]
   # end
 
-  # def display_items
-  #   array = []
-  #   array << ["Description", "Quantity", { content: "Price", align: :right }]
-  #   order_items(array)
-  #   array << total_price
-  #   table(array, column_widths: [340, 75, 125]) do
-  #     cells.borders = []
-  #     row(0).borders = [:bottom]
-  #   end
-  # end
-
-  # def order_items(array)
-  #   @order.order_items.each do |item|
-  #     array << [
-  #       item.description,
-  #       item.quantity,
-  #       { content: number_to_currency(item.price), align: :right }
-  #     ]
-  #   end
-  # end
 
   # def total_price
   #   [
