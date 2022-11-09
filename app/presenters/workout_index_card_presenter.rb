e4d94cfc7e72d9cd583b2baf50a9ce81cb7ddcc9
Stephen Schuez <stephen.schuez@gmail.com>
2022-11-09 09:43:56 +0100

    Finish IndexCardPresenter

--- app/presenters/workout_index_card_presenter.rb
+++ app/presenters/workout_index_card_presenter.rb
@@ -2,8 +2,7 @@ class WorkoutIndexCardPresenter
   include ActionView::Context
   include ActionView::Helpers::AssetTagHelper
   include ActionView::Helpers::UrlHelper
-  include Rails.application.routes.url_helpers
-  
+
   def self.call(workout, view_context)
     new(workout, view_context).call
   end
@@ -17,23 +16,19 @@ class WorkoutIndexCardPresenter
   def call
     card  
   end
-
+  
   private
-
+  
   def card
     content_tag :div, id: "workout_#{workout.id}", class: "card workout-index-card" do
-      # view_context.link_to workout_url(workout) do
-      # Rails.application.routes.url_helpers.workout_path(workout) do
-      # link = link_to(Rails.application.routes.url_helpers.workout_path(workout))
-      # content_tag :a, link do
-        workout_card
-      # end
-      # card_controls      
+      link_to(view_context.workout_url(workout)) do
+        card_content
+      end +
+      card_controls      
     end
   end
 
-  def workout_card
-    # turbo_stream_from workout
+  def card_content
     content_tag :div, class: "card-header bg-transparent small" do
       card_title +
       workout_description +
@@ -54,9 +49,30 @@ class WorkoutIndexCardPresenter
     content_tag :p, workout_st, class: "text-secondary small grey my-1"
   end
 
+  def card_controls
+    return unless current_user_admin_or_user?
+
+    content_tag :div, class: "workout-edit meta", data: { "meta-target": "user" } do
+      delete_button
+    end
+  end
+
+  def current_user_admin_or_user?
+    view_context.current_user && 
+      (view_context.current_user == workout.user || view_context.current_user&.admin?)
+  end
+
+  def delete_button
+    button_to(view_context.workout_url(workout),
+      method: :delete,
+      form: { data: { turbo_confirm: "Are you sure you want to delete this workout?" } },
+      class: "btn-styling") do 
+        content_tag :span, "🗑️", class: "emoji"
+      end
+  end
+
   def workout_blocks
-    ActionController::Base.helpers.pluralize(workout.blocks.count, 'block') 
-    # pluralize workout.blocks.count, "block"
+    ActionController::Base.helpers.pluralize(workout.blocks.count, 'block')
   end
 
 end
