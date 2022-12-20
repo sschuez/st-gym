f1226b79865a951e6e182a06d947da1c9d055db9
Stephen Schuez <stephen.schuez@gmail.com>
2022-12-20 15:30:38 +0100

    Refactor of dark mode to be more stimulus

--- app/javascript/controllers/dark_controller.js
+++ app/javascript/controllers/dark_controller.js
@@ -4,42 +4,38 @@ export default class extends Controller {
   static targets = [ "btn" ]
 
   connect() {
-    var theme = getCookie("theme")
-    if (theme == "light-mode") {
+    this.theme = this.getCookie("theme")
+
+    if (this.theme == "light-mode") {
       this.btnTarget.innerHTML = "🌘"
       this.btnTarget.nextElementSibling.innerHTML = "Dark Mode"
     } else {
       this.btnTarget.innerHTML = "🌞"
       this.btnTarget.nextElementSibling.innerHTML = "Light Mode"
-    }
+    }  
+  }
 
-  
+  getCookie(cname) {
+    this.name = cname + "="
+    this.ca = document.cookie.split(';')
 
-    // Get cookie - for reference only (cosole.log())
-    function getCookie(cname) {
-      let name = cname + "=";
-      let ca = document.cookie.split(';');
-      for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-        }
+    for(let i = 0; i < this.ca.length; i++) {
+      let c = this.ca[i]
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1)
       }
-      return "";
+      if (c.indexOf(this.name) == 0) {
+        return c.substring(this.name.length, c.length)
+      }
     }
-
+    return ""
   }
 
   darkMode() {
-    var element = document.body
-    element.classList.toggle("dark-mode")
+    document.body.classList.toggle("dark-mode")
+    this.currentTheme = document.body.classList.contains("dark-mode") ? "dark-mode" : "light-mode"
 
-    // Cookies toggle
-    let currentTheme = element.classList.contains("dark-mode") ? "dark-mode" : "light-mode"
-    if (currentTheme == "dark-mode") {
+    if (this.currentTheme == "dark-mode") {
       document.body.classList.remove("light-mode")
       document.cookie = "theme=dark-mode"
       this.btnTarget.innerHTML = "🌞"
