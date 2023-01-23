53759f21dd3ed3282f656d66f10d9af4a9841d51
Stephen Schuez <stephen.schuez@gmail.com>
2023-01-23 16:34:56 +0100

    Make email optional for feedbacks

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  # validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :content, presence: true
 
   scope :ordered, -> { order(created_at: :desc) }
