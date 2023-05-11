68efb95653e7c1bb2147d903c53e2a2eaacc3e15
Stephen Schuez <stephen.schuez@gmail.com>
2023-05-11 18:23:21 +0900

    Add migration for join table exercise_category

---
+++ db/migrate/20230511092240_create_exercise_categories.rb
@@ -0,0 +1,10 @@
+class CreateExerciseCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :exercise_categories do |t|
+      t.references :exercise, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
