b26ef6141c5de9ff5837aaffdf6ce98b7155298a
Stephen Schuez <stephen.schuez@gmail.com>
2023-05-12 10:16:36 +0900

    Add first test to visit homepage

---
+++ spec/factories/workouts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :workout do
+    id { 1 }
+    name { 'Test workout' }
+    description { 'This is a test workout, with id 7' }
+  end
+end
+
--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -3,9 +3,11 @@ require "rails_helper"
 feature "User visits homepage" do
   scenario "successfully" do
     user = create(:user, admin: true)
-    article = create(:article, title: "Published article", summary: "This is the summary of a published test article", published: true, user: user)
+    workout_1 = create(:workout, id: 7, name: "Some workout", description: "This is a test workout", user: user)
+    workout_2 = create(:workout, id: 5, name: "Some workout", description: "This is a test workout", user: user)
+    workout_3 = create(:workout, id: 4, name: "Some workout", description: "This is a test workout", user: user)
 
     visit root_path
-    expect(page).to have_css 'h1', text: 'Tool'
+    expect(page).to have_css 'h1', text: 'Workout'
   end
 end
