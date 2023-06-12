6b6bb6ebe63b1cea7923272929ae6ebdae872301
Stephen Schuez <stephen.schuez@gmail.com>
2023-06-12 14:16:26 +0200

    Change styling of main-cat pills

--- app/controllers/exercise_instances_controller.rb
+++ app/controllers/exercise_instances_controller.rb
@@ -12,51 +12,55 @@ class ExerciseInstancesController < ApplicationController
   def new
     @exercise_instance = @block.exercise_instances.new
     authorize @exercise_instance
-    
-    main_category_query = params[:main_category].present? && (params[:main_category].to_i != 0 || params[:main_category] != "reset_main_category_list")
 
-    category_query = params[:category].present? && (params[:category].to_i != 0 || params[:category] != "reset_category_list")
-    
-    if main_category_query && category_query
-      main_category_exercises = Exercise.joins(:exercise_categories).where(exercise_categories: { category_id: params[:main_category].to_i }).ordered
-      category_exercises = Exercise.joins(:exercise_categories).where(exercise_categories: { category_id: params[:category].to_i }).ordered
-      exercises_array = main_category_exercises & category_exercises
-      @exercises = Exercise.where(id: exercises_array.map(&:id))
-    elsif main_category_query && !category_query
-      @exercises = Exercise.joins(:exercise_categories).where(exercise_categories: { category_id: params[:main_category].to_i }).ordered
-    elsif params[:main_category] == "reset_main_category_list" && category_query
-      @exercises = Exercise.joins(:exercise_categories).where(exercise_categories: { category_id: params[:category].to_i }).ordered  
-    else
-      @exercises = Exercise.ordered
-    end
+    @exercises = determine_category_exercises(params)
     query_and_respond(@exercises)
   end
   
   def create
     exercises = params[:exercise_instance][:exercise_id]
     @exercise_instances = exercises.reject { |e| e.empty? }
-                                    .map do |exercise|      
+    .map do |exercise|      
       @exercise_instance = @block.exercise_instances.create(
         exercise: Exercise.find(exercise.to_i)
       )
       authorize @exercise_instance
     end
-      
+    
     respond_to { |format| format.turbo_stream }
   end
 
   def destroy
     @exercise_instance = ExerciseInstance.find(params[:id])
     authorize @exercise_instance
-
+    
     @exercise_instance.destroy
-
+    
     respond_to do |format|
       format.turbo_stream
     end
   end
-
+  
   private
+  
+  def determine_category_exercises(params)
+    main_category_query = params[:main_category].present? && (params[:main_category].to_i != 0 || params[:main_category] != "reset_main_category_list")
+    category_query = params[:category].present? && (params[:category].to_i != 0 || params[:category] != "reset_category_list")
+    
+    if main_category_query && category_query
+      main_category_exercises = Exercise.joins(:exercise_categories).where(exercise_categories: { category_id: params[:main_category].to_i }).ordered
+      category_exercises = Exercise.joins(:exercise_categories).where(exercise_categories: { category_id: params[:category].to_i }).ordered
+      exercises_array = main_category_exercises & category_exercises
+      return exercises = Exercise.where(id: exercises_array.map(&:id))
+    elsif main_category_query && !category_query
+      return exercises = Exercise.joins(:exercise_categories).where(exercise_categories: { category_id: params[:main_category].to_i }).ordered
+    elsif params[:main_category] == "reset_main_category_list" && category_query
+      return exercises = Exercise.joins(:exercise_categories).where(exercise_categories: { category_id: params[:category].to_i }).ordered  
+    else
+      return exercises = Exercise.ordered
+    end
+  end
+
 
   def query_and_respond(exercises)
     if params[:query].present? && params[:query].to_i == 0
