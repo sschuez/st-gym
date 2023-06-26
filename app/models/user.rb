5a8b2749da61c2fd07b1014369284a4df5c7216d
Stephen Schuez <stephens@hey.com>
2023-06-26 21:32:44 +0200

    Add user scopes for categories and exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,30 @@ class User < ApplicationRecord
 
   has_many :workouts
   has_many :exercises, dependent: :nullify
+  has_many :categories, through: :exercises
   has_many :visits, class_name: "Ahoy::Visit"
+
+  def main_categories
+    categories.main_categories
+  end
+
+  def other_categories
+    categories.other_categories
+  end
+
+  def main_categories_with_exercises
+    categories.main_categories_with_exercises
+  end
+
+  def exercises_per_category(category_id)
+    exercises.by_category(category_id)
+  end
+
+  def exercises_per_main_category(main_category_id)
+    exercises.by_main_category(main_category_id)
+  end
+
+  def exercises_per_main_and_other_category(main_category_id, other_category_id)
+    exercises.by_main_and_other_category(main_category_id, other_category_id)
+  end
 end
